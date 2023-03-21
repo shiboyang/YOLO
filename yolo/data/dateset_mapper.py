6ff99515da03364c387330c45fdfe7d3dee7a9a8
shiboyang <sby089@126.com>
2023-03-21 21:21:37 +0800

    :construction: dataset mapper

---
+++ yolo/data/dateset_mapper.py
@@ -0,0 +1,34 @@
+# @Time    : 2023/3/21 上午10:26
+# @Author  : Boyang
+# @Site    : 
+# @File    : dateset_mapper.py
+# @Software: PyCharm
+from typing import List, Union, Optional
+
+import numpy as np
+
+from detectron2.data.dataset_mapper import DatasetMapper
+import detectron2.data.transforms as T
+from detectron2.config import configurable
+
+
+class YOLOV3DatasetMapper:
+    @configurable
+    def __init__(
+            self,
+            is_train: bool,
+            *,
+            augmentations: List[Union[T.Augmentation, T.Transform]],
+            image_format: str,
+            use_instance_mask: bool = False,
+            use_keypoint: bool = False,
+            instance_mask_format: str = "polygon",
+            keypoint_hflip_indices: Optional[np.ndarray] = None,
+            precomputed_proposal_topk: Optional[int] = None,
+            recompute_boxes: bool = False
+    ):
+        ...
+
+    @classmethod
+    def from_config(cls, cfg, is_tran: bool = True):
+        aug = build_augmentaion
